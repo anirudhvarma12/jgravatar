6915585aab05ea2ba80be8030c475b437ae0d513
Anirudh <varma93@hotmail.com>
2014-11-29 13:08:21 +0530

    Added newer package files.

---
+++ src/main/java/com/github/anirudhvarma12/jgravatar/GravatarDefaultImage.java
@@ -0,0 +1,31 @@
+package com.github.anirudhvarma12.jgravatar;
+
+public enum GravatarDefaultImage {
+
+	GRAVATAR_ICON(""),
+
+	IDENTICON("identicon"),
+
+	MONSTERID("monsterid"),
+
+	WAVATAR("wavatar"),
+
+	HTTP_404("404"),
+        
+        MYSTERY_MAN("mm"),
+        
+        RETRO("retro"),
+        
+        BLANK("blank");
+
+	private String code;
+
+	private GravatarDefaultImage(String code) {
+		this.code = code;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+}
